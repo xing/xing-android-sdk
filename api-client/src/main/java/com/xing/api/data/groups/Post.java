0cfc0dd2905901ec56e3a6c21be9390cd05c3402
Daniel Hartwich <daniel.hartwich@xing.com>
2016-08-22 11:51:58 +0200

    Add empty models for groups

---
+++ api-client/src/main/java/com/xing/api/data/groups/Post.java
@@ -0,0 +1,10 @@
+package com.xing.api.data.groups;
+
+import java.io.Serializable;
+
+/**
+ * @author dhartwich1991
+ */
+public class Post implements Serializable {
+    private static final long serialVersionUID = 1L;
+}
