ccf449f58ee02438667860f1f555276bc927d593
Shyish <shyballano@gmail.com>
2018-01-08 19:00:09 +0100

    Converted messages models.

---
+++ api-client/src/main/java/com/xing/api/data/messages/ConversationMessage.kt
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2016 XING SE (http://xing.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.xing.api.data.messages
+
+import com.squareup.moshi.Json
+import com.xing.api.data.SafeCalendar
+import com.xing.api.data.profile.XingUser
+import java.io.Serializable
+
+/**
+ * Java representation of a Message in a Conversation.
+ */
+data class ConversationMessage(
+        @Json(name = "id")
+        val messageId: String,
+
+        @Json(name = "created_at")
+        val createdAt: SafeCalendar,
+
+        @Json(name = "content")
+        val content: String,
+
+        @Json(name = "read")
+        val isRead: Boolean,
+
+        @Json(name = "sender")
+        val sender: XingUser,
+
+        @Json(name = "attachments")
+        val attachments: List<MessageAttachment>?
+) : Serializable {
+
+    override fun equals(other: Any?): Boolean  = other is ConversationMessage && messageId == other.messageId
+
+    override fun hashCode(): Int = messageId.hashCode()
+
+    companion object {
+        private const val serialVersionUID = -4307893362880528921L
+    }
+}
---
+++ api-client/src/main/java/com/xing/api/data/messages/MessageAttachment.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2016 XING SE (http://xing.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.xing.api.data.messages
+
+import com.squareup.moshi.Json
+import com.xing.api.data.SafeCalendar
+import java.io.Serializable
+
+/**
+ * Java representation of an Attachment on the Message.
+ */
+data class MessageAttachment(
+        @Json(name = "id")
+        val id: String,
+
+        @Json(name = "created_at")
+        val createdAt: SafeCalendar,
+
+        @Json(name = "filename")
+        val fileName: String,
+
+        @Json(name = "mime_type")
+        val mimeType: String,
+
+        @Json(name = "size")
+        val fileSize: Int
+) : Serializable {
+
+    override fun equals(other: Any?):Boolean = other is MessageAttachment && id == other.id
+
+    override fun hashCode(): Int = id.hashCode()
+
+    companion object {
+        private const val serialVersionUID = 1L
+    }
+}
