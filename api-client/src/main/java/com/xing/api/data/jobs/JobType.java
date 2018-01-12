840bba1777f24822c591c2358e147ece1f237fe5
Alberto Ballano <aballano@users.noreply.github.com>
2018-01-12 16:20:20 +0100

    Using a normal C for the copyright symbol (#228)

--- api-client/src/main/java/com/xing/api/data/jobs/JobType.java
+++ api-client/src/main/java/com/xing/api/data/jobs/JobType.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (С) 2018 XING SE (http://xing.com/)
+ * Copyright (C) 2018 XING SE (http://xing.com/)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
